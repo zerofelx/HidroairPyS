eb5349edbed1f5730b125a11f0a0297bf26036f0
Jesus David <zeroflex3@gmail.com>
2019-03-04 11:10:56 -0500

    Agregando yo que wa saber lrpm

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,10 +1,13 @@
 function menuComputador(lugarNE, lugarAA, lugarRC, lugarPE, lugarC) {
     return `
-        <a href="${lugarNE}" class="botonesMenu">Nuestra Empresa</a>
-        <a href="${lugarAA}" class="botonesMenu">Aires Acondicionados</a>
-        <a href="${lugarRC}" class="botonesMenu">Redes y Comunicaciones</a>
-        <a href="${lugarPE}" class="botonesMenu">Plantas Eléctricas</a>
-        <a href="${lugarC}" class="botonesMenu">Contactenos</a>
+        <div style="margin: 10px 0 20px 0;">
+            <a href="${lugarNE}" class="botonesMenu">Nuestra Empresa</a>
+            <a href="${lugarAA}" class="botonesMenu">Aires Acondicionados</a>
+            <a href="${lugarRC}" class="botonesMenu">Redes y Comunicaciones</a>
+            <a href="${lugarPE}" class="botonesMenu">Plantas Eléctricas</a>
+            <a href="${lugarC}" class="botonesMenu">Contactenos</a>
+
+        </div>
     `
 }
 
